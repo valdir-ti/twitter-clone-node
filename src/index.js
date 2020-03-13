6fa8f5fea1807777c60ad9132ea35e558e95cce2
valdir-ti <valdir.ti@gmail.com>
2020-03-13 15:50:12 -0300

    Inserção do arquivo utilizado no InSomnia para teste das rotas

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ mongoose.connect("mongodb://localhost:27017/twitter-node", {
   useUnifiedTopology: true
 });
 
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: `http://localhost:${port}` }));
 
 app.use((req, res, next) => {
   req.io = io;
