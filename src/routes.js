883936c9f244aab4482e4cce9f48f55e6cc3fa86
valdir-ti <valdir.ti@gmail.com>
2020-03-13 13:45:50 -0300

    Inserindo um like no tweet pelo id

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,14 @@ const express = require("express");
 const routes = express.Router();
 
 const TweetController = require("./controllers/TweetController");
+const LikeController = require("./controllers/LikeController");
 
 routes.get("/", (req, res) => {
   return res.send("Página inicial");
 });
 routes.get("/tweets", TweetController.index);
 routes.post("/tweets", TweetController.store);
+
+routes.post("/likes/:id", LikeController.store);
 
 module.exports = routes;
